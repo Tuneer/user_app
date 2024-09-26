4765317b9dd8ebcd6fdf7a8289cfff49578532cf
Tuneer Mahatpure <mahatpuretuneer@gmail.com>
2024-09-26 23:57:31 +0530

    salon app
    Booking appointment app salon for jsc

---
+++ android/app/src/main/kotlin/com/multisalon/user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.multisalon.user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/env.dart
@@ -0,0 +1,15 @@
+/*
+  Authors : Sunil
+  Website : https://jainodaya.info/
+  App Name : Ultimate Salon Full App Flutter V3
+  This App Template Source code is licensed as per the
+  terms found in the Website http://localhost/license
+  Copyright and Good Faith Purchasers © 2024-present ASB.
+*/
+class Environments {
+  static const String appName = 'Multi-Salon';
+  static const String companyName = 'JSC Global Solutions';
+  static const String googleMapsKey = 'AIzaSyCCcuvNC3mxmCH-AXy-6RJN7-9zGB60LfM';
+  static const String apiBaseURL = 'http://jainodaya.info/multisalon/api/public/';
+  static const String websiteURL = 'http://jainodaya.info/multisalon/api/web/';
+}
